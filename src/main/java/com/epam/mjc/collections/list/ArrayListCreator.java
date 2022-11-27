dfb7f4c60ed239ad11881958dd10ad5f7115de5f
atainaive <atai.omorov@stud.vgtu.lt>
2022-11-27 12:30:30 +0200

    done

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,18 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        int index = 1;
+
+        for (String s : sourceList) {
+            if (index % 3 == 0) {
+                arrayList.add(s);
+                arrayList.add(s);
+            }
+            index++;
+        }
+
+        return arrayList;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,19 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int numberA = Integer.parseInt(a);
+        int numberB = Integer.parseInt(b);
+
+        int funcA = 5 * numberA*numberA +3;
+        int funcB = 5 * numberB*numberB +3;
+
+        return funcA == funcB ? numberA - numberB : funcA - funcB;
     }
 }
